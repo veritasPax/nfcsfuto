7c25d4ebe307310b69fd73f53f1e7ff69bebed31
Anthony Achonye <tonexify@gmail.com>
2017-09-16 02:06:42 +0100

    autoload url and form helper functions

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,8 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array("database");
-
+$autoload['libraries'] = array('database');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Drivers
@@ -89,7 +88,10 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+    'url',
+    'form'
+);
 
 /*
 | -------------------------------------------------------------------
