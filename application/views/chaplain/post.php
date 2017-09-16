e27c79c854aea36c05df7ee5e28e8739ce4bd5b6
Anthony Achonye <tonexify@gmail.com>
2017-09-16 02:05:14 +0100

    template for post display working

---
+++ application/views/chaplain/post.php
@@ -0,0 +1,112 @@
+<div class="row">
+			<!--left area-->
+			<div class="col-md-8 clearfix left-area">
+				<div class="">
+				<h2 class="page-header"><?php echo $post['title']; ?></h2>
+				<div>
+					<small>
+						<span class="text-primary"><i class="fa fa-edit"></i> &nbsp; Rev. Fr. Jude Ike</span>&nbsp;&nbsp;
+						<span><i class="fa fa-calendar"></i>&nbsp; 12th September, 2017<span>&nbsp;&nbsp;
+						<span class="text-primary"><i class="fa fa-comment"></i>&nbsp;3 comments</span>
+					</small>
+				</div>
+				<br>
+				<div class="media">
+					<img class="post-object pull-right" src="<?php echo base_url('assets/img/church-window-baptism-sacrament-glass-window (1).jpg'); ?>"/>
+					<div class="">
+					<p class="read-content">
+						<?php echo $post['content']; ?>
+					</p>
+					</div>
+				</div>
+				<br>
+				<h3>Comments(3)</h3>
+				<div class="comment-area row">
+					<div class="comment-box">
+						<div class="comment-id">
+							<img src="images/default-user.png" class="img-thumbnail pull-right media-object"/>
+							<h5><b>Anthony</b></h5>
+							<h6><small>September 13, 2017 at 4:10pm</small></h6>
+						</div>
+						<p>
+							The pulpit is for the preaching the Gospel, not for fraternal correction.��the Mass is not an occasion for the preacher to address some issue completely unrelated to the liturgical celebration and its readings, or to do violence to the texts provided by the Church by twisting them to fit some preconceived idea. Nor is the homily simply an exercise in biblical exegesis. �Next, the homily is not catechetical instruction, even if catechesis is an important dimension of the homily. Finally, the time for the homily should not be taken up with the preacher�s personal witness. There is no question that people can be deeply moved by personal stories, but the homily should express the faith of the Church, and not simply the preacher�s own story. The same Spirit who in
+						</p>
+						<h4 class="text-info"><a href="">Reply</a></h4>
+					</div>
+				</div>
+
+				<h3 class="">Leave a Reply</h3>
+				<form method="" class="reply-area row">
+					<div class="row">
+						<div class="col-sm-12">
+							
+							<div class="col-md-6 col-sm-12">
+								<input type="text" class="form-control"/>
+							</div>
+							<div class="col-md-6 col-sm-12">
+								<label class="text-muted">Name (Required)</label>
+							</div>
+						</div>
+
+						<div class="col-sm-12">
+							<div class="col-md-6 col-sm-12">
+								<input type="text" class="form-control"/>
+							</div>
+							<div class="col-md-6 col-sm-12">
+								<small class="form-text text-muted">Email ( Required; will not be published )</label>
+							</div>
+						</div>
+
+						<div class="col-sm-12">
+							<div class="col-md-6 col-sm-12">
+								<input type="text" class="form-control"/>
+							</div>
+							<div class="col-md-6 col-sm-12">
+								<label class="text-muted">Website</label>
+							</div>
+						</div>
+
+						<div class="col-sm-12">
+							<div class="col-md-12 col-sm-12">
+								<textarea class="form-control"></textarea>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="col-md-12">
+							<button class="reply-btn" title="comment">Submit Comment</button>
+							</div>
+						</div>
+					</div>
+				</form>
+				</div>
+			</div>
+			<!--right area-->
+			<div class="col-md-4 clearfix right-area">
+				<h4 class="page-header">Join the conversation</h4>
+				<p class="text-muted">Connect with us on the following social-media platforms</p>
+				<div align="center">
+					<a class="btn btn-sm fb-btn" title="facebook"><span class="fa fa-facebook"></span></a>
+					<a class="btn btn-sm tw-btn" title="twitter"><span class="fa fa-twitter"></span></a>
+					<a class="btn btn-sm wh-btn" title="whatsapp"><span class="fa fa-whatsapp"></span></a>
+				</div>
+
+				<h4 class="page-header">Subscribe</h4>
+				<h5 class="text-muted">Enter email address below to receive updates each time new time is published!</h5>
+				<form method="" class="newsletter">
+					<div class="form-group">
+						<input type="email" placeholder="Email Address"/>
+						<button class="">Submit</button>
+						<div><small class="text-muted">Privacy guaranteed. We never share your info.</small></div>
+					</div>
+				</form>
+
+				<h4 class="page-header">Search</h4>
+				<form method="" class="newsletter">
+					<div class="form-group">
+						<input type="email" placeholder="Email Address"/>
+						<button class="">Search</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
